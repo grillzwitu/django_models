8a1cc5fa698f70acc93d83f0825ecedae062a288
grillzwitu <grillzwitu@gmail.com>
2022-06-17 10:26:53 +0100

    tested create and delete Post object

--- I4G007845WRJ/blog/models.py
+++ I4G007845WRJ/blog/models.py
@@ -4,10 +4,14 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 
 class Post(models.Model):
-
+    """Defines A Blog Post"""
     title = models.CharField(max_length=200)
     text = models.TextField()
     User = get_user_model()
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     created_date = models.DateTimeField()
     published_date = models.DateTimeField()
+
+    def __str__(self) -> str:
+        """Overrides deafault __str__ method, Prints title of the blog"""
+        return self.title
